d68b28be1b228a412518fbc97fe84917868eef5f
OutsystemsNOS <45230356+OutsystemsNOS@users.noreply.github.com>
2021-01-22 19:42:17 +0000

    Create androidBeforeInstall.js

---
+++ scripts/configurations/androidBeforeInstall.js
@@ -0,0 +1,48 @@
+ "use strict";
+
+var utils = require("utilities");
+
+module.exports = function(ctx) {
+    var fs = ctx.requireCordovaModule('fs'),
+        path = ctx.requireCordovaModule('path'),
+        xml = ctx.requireCordovaModule('cordova-common').xmlHelpers;
+   
+  var cordovaAbove8 = utils.isCordovaAbove(context, 8);
+  var cordovaAbove7 = utils.isCordovaAbove(context, 7);
+  var defer;
+  if (cordovaAbove8) {
+    defer = require("q").defer();
+  } else {
+    defer = context.requireCordovaModule("q").defer();
+  }
+  
+  var platform = context.opts.plugin.platform;
+  var platformConfig = utils.getPlatformConfigs(platform);
+  if (!platformConfig) {
+    utils.handleError("Invalid platform", defer);
+  }
+  console.log("---DEBUG BUILD----");
+  var wwwPath = utils.getResourcesFolderPath(context, platform, platformConfig);
+  console.log(wwwPath);
+  var sourceFolderPath = utils.getSourceFolderPath(context, wwwPath);
+  var configFilePath = path.join(context.opts.projectRoot, "www",'AndroidManifest.xml');
+  console.log(configFilePath);
+    
+    console.log("---DEBUG END----");
+    /*
+    var manifestPath = path.join(ctx.opts.projectRoot, 'platforms/android/AndroidManifest.xml');
+    var doc = xml.parseElementtreeSync(manifestPath);
+    if (doc.getroot().tag !== 'manifest') {
+        throw new Error(manifestPath + ' has incorrect root node name (expected "manifest")');
+    }
+
+    //adds the tools namespace to the root node
+    doc.getroot().attrib['xmlns:tools'] = 'http://schemas.android.com/tools';
+    
+    //add attribute android:supportsRtl
+    doc.getroot().find('./application').attrib['android:supportsRtl'] = 'true';
+
+    //write the manifest file
+    fs.writeFileSync(manifestPath, doc.write({indent: 4}), 'utf-8');*/
+
+};
